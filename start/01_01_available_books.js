a699cc9a65a7c1ab901de11106464f4bfac6ea29
refwil <66493859+refwil@users.noreply.github.com>
2024-03-09 00:21:10 +0000

    Committing intial changes

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,66 @@
 // Write your code here
+// function Book(title, author, ISBN, numCopies){
+//   this.title = title;
+//   this.author = author;
+//   this.ISBN = ISBN;
+//   this.numCopies = numCopies;
+// }
+
+// Book.prototype.getAvailability = function() {
+//   if (this.numCopies < 10){
+//     return "Low stock"
+//   }
+//   else if (this.numcopies === 0) {
+//     return "Out of stock"
+
+//   }
+//   return "in stock"
+// }
+
+// Book.prototype.sell = function(numCopiesSold = 1) {
+//   this.numCopies -= numCopiesSold;
+// }
+
+// Book.prototype.restock = function(numCopiesStocked = 5) {
+//   this.numCopies += numCopiesStocked;
+// }
+
+class Book {
+  constructor(title, author, ISBN, numCopies){
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;    
+  }
+
+  get availability(){
+    return this.getAvailability();
+  }
+
+  getAvailability(){
+    if (this.numCopies < 10){
+      return "Low stock";
+    }
+    else if (this.numcopies === 0) {
+      return "Out of stock";
+
+    }
+    return "In stock";
+  }
+
+  sell(numCopieSold = 1){
+    this.numCopies -= numCopieSold;
+  }
+  restock(numCopiesStocked = 5){
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.availability);
+
+HungerGames.restock(12);
+console.log(HungerGames.availability);
+
+HungerGames.sell(17);
+console.log(HungerGames.availability);
--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -31,3 +31,14 @@ class Book {
 }
 
 // Write your code here
+
+class TechnicalBook extends Book {
+  constructor(title, author, ISBN, numCopies, edition) {
+    super(title, author, ISBN, numCopies)
+    this.edition = edition;
+  }
+
+  getEdition(){
+    return `The current version of this book is ${this.edition}`
+  }
+}
