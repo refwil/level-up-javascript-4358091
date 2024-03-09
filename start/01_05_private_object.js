f51408cd5706763d1ba090eae1b11209f93cab1d
refwil <66493859+refwil@users.noreply.github.com>
2024-03-09 00:57:37 +0000

    Additional code work

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,11 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "wilson",
+  [password]: "1234",
+  age: 40
+}
+console.log(user.username);
+console.log(user.password);
--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,17 @@
 // Write your code here
+function getStudents(classroom) {
+  let { hastTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (hastTeachingAssistant){
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}));
